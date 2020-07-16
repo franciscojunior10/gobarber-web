4c949ad1896091c6b1b407b7a1e1006a6df810aa
Francisco Junior <franciscojn49@gmail.com>
2020-07-16 16:51:52 -0300

    feat: added validation with the lib Yup in the register User and Logon User.

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
+import { Form } from '@unform/web';
+import * as Yup from 'yup';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
@@ -9,12 +11,32 @@ import { Container, Content, Background } from './styles';
 import logo from '../../assets/logo.svg';
 
 const SignIn: React.FC = () => {
+  const handleSubimit = useCallback(async (data: object) => {
+    try {
+      const schema = Yup.object().shape({
+        email: Yup.string()
+          .email('Digite um e-mail válido.')
+          .required('E-mail obrigátorio.'),
+        password: Yup.string().min(
+          6,
+          'Senha deve conter no mínino 6 caracteres.',
+        ),
+      });
+
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  }, []);
+
   return (
     <Container>
       <Content>
         <img src={logo} alt="Logo" />
 
-        <form action="">
+        <Form onSubmit={handleSubimit}>
           <h1>Faça seu logon</h1>
 
           <Input name="email" icon={FiMail} placeholder="E-mail" />
@@ -28,7 +50,7 @@ const SignIn: React.FC = () => {
           <Button type="submit">Entrar</Button>
 
           <a href="forgot">Esqueci minha senha</a>
-        </form>
+        </Form>
 
         <a href="login">
           <FiLogIn size={20} /> Criar conta
--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { FiArrowLeft, FiMail, FiLock, FiUser } from 'react-icons/fi';
 import { Form } from '@unform/web';
+import * as Yup from 'yup';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
@@ -10,9 +11,26 @@ import { Container, Content, Background } from './styles';
 import logo from '../../assets/logo.svg';
 
 const SignUp: React.FC = () => {
-  function handleSubimit(data: object): void {
-    console.log(data);
-  }
+  const handleSubimit = useCallback(async (data: object) => {
+    try {
+      const schema = Yup.object().shape({
+        name: Yup.string().required('Nome obrigatório.'),
+        email: Yup.string()
+          .email('Digite um e-mail válido')
+          .required('E-mail obrigatório.'),
+        password: Yup.string().min(
+          6,
+          'Senha deve conter no mínimo 6 caracteres.',
+        ),
+      });
+
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  }, []);
 
   return (
     <Container>
